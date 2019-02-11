3b2016106da543798c847622fde70eade378884d
Sudarshanamicky <43535468+Sudarshanamicky@users.noreply.github.com>
2019-02-11 21:42:03 +0530

    Update n.c

--- n.c
+++ n.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=0;
+    int n,i,s=0;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        sum=sum+i;
+        s=s+i;
     }
-    printf("%d",sum);
+    printf("%d",s);
     return 0;
 }
